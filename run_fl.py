b639d15f619be953d05bae3488a5fd6ba9c4225e
Eve Hu <yydxhhuyi@gmail.com>
2024-12-15 16:48:31 -0500

    model update

--- run_fl.py
+++ run_fl.py
@@ -312,10 +312,10 @@ if __name__ == '__main__':
         args.adaptive_bitwidth = True
     elif args.algorithm == 'FedQT':
         args.qmode = 1
-        args.update_mode = 1
+        args.update_mode = 0
     elif args.algorithm == 'FedQT-BA':
         args.qmode = 1
-        args.update_mode = 1
+        args.update_mode = 0
         args.adaptive_bitwidth = True
     else:
         raise ValueError("Algorithm not supported")
--- src/model.py
+++ src/model.py
@@ -272,10 +272,10 @@ def NITI_weight_update(w, ws, g, gs, m, range):
     
 def fp_weight_update(w, ws, g, gs, bitwidth, bs, lr):
     wn = w * ws[0]
-    g = g * gs[0] * lr/bs
+    g = g * gs[0] * lr/bs  
     if g.abs().max() > wn.abs().max()/4:
         g = g * wn.abs().max()/g.abs().max() / 4
-    wn = wn - g
+    wn = w - g
     wt, scale = fp_quant_stochastic(wn, bitwidth)
     ws[0] = scale[0]
     return int8_clip(wt, 2**bitwidth - 1)
