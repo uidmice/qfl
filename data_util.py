15c40d58913495f9abcd409196e0b28048ed976e
Eve Hu <yydxhhuyi@gmail.com>
2025-04-04 22:10:31 -0400

    fix niid

--- data_util.py
+++ data_util.py
@@ -52,7 +52,7 @@ def random_split_clients(dataset, n_clients):
     return client_datasets
 
 def dirichlet_sample(dataset, num_clients, num_classes, alpha=0.5):
-    labels = np.array(dataset.dataset.targets)[dataset.indices.astype(int)]
+    labels = np.array(dataset.targets)[dataset.indices.astype(int)]
     
     # Create a list of indices for each class
     indices_per_class = [[] for _ in range(num_classes)]
